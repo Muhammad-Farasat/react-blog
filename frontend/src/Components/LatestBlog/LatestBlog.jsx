5adc1fd4775d5935aa19be29d578b3fb5d5cd6e7
Muhammad-Farasat <farasatkhan687@gmail.com>
2025-01-03 23:19:35 +0500

    updates

--- frontend/src/Components/LatestBlog/LatestBlog.jsx
+++ frontend/src/Components/LatestBlog/LatestBlog.jsx
@@ -1,44 +1,58 @@
-import React, {useState, useEffect} from 'react'
-import Card from '../Card/Card'
+import React, { useState, useEffect } from "react";
+import Card from "../Card/Card";
+import Loader from "../Loader/Loader";
 
 function LatestBlog() {
+  const [newBlog, setNewBlog] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const backend_url = import.meta.env.VITE_BACKEND_URL;
 
-    const [newBlog, setNewBlog] = useState([])
-    const backend_url = import.meta.env.VITE_BACKEND_URL
+  useEffect(() => {
+    setLoading(true)
+    try {
+        fetch(`${backend_url}/latestblog`)
+          .then((response) => response.json())
+          .then((data) => setNewBlog(data));
+    } catch (error) {
+        console.error(error);
+    }finally{
+        setLoading(false)
+    }
+  }, []);
 
-    useEffect(() => {
-      
-      fetch(`${backend_url}/latestblog`)
-      .then((response)=>response.json())
-      .then((data)=>setNewBlog(data))
+  return (
+    <>
+      <section className=" h-[100vh] py-16">
+        <div className="container mx-auto">
+          <h3 className="text-3xl text-center font-bold text-[#111827] mb-8">
+            Latest Blogs
+          </h3>
 
-    }, [])
-    
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+              {/* Blog Card */}
 
-    return (
-        <>
-            <section className=" h-[100vh] py-16">
-                <div className="container mx-auto">
-                    <h3 className="text-3xl text-center font-bold text-[#111827] mb-8">Latest Blogs</h3>
+              {newBlog.map((item, i) => {
+                return (
+                  <Card
+                    key={i}
+                    title={item.title}
+                    description={item.description}
+                    image={item.image}
+                    id={item._id}
+                  />
+                );
+              })}
 
-
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                        {/* Blog Card */}
-
-                        { newBlog.map((item, i)=>{
-                            return <Card key={i} title={item.title} description={item.description} image={item.image} id={item._id} />
-                            })
-                        }
-
-                        
-                        {/* Repeat Blog Cards */}
-                    </div>
-
-
-                </div>
-            </section>
-        </>
-    )
+              {/* Repeat Blog Cards */}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default LatestBlog
+export default LatestBlog;
